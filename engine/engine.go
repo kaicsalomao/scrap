f0e77935b3b109b3f714c0920f4f40ecf5fcc12c
Kaic Salomão <imkaicsm@gmail.com>
2024-03-06 20:24:30 -0300

    Removendo comentários desnecessários e redundantes
    Baseando-se no livro Clean Code por Robert C. Martin

--- engine/engine.go
+++ engine/engine.go
@@ -7,24 +7,23 @@ import (
 	"strings"
 )
 
-// Reprenseta a Engine e sua estrutura de chamada
 type Engine struct {
 	url      string
 	keywords []string
 }
 
-// Cria um ponteiro para a Engine
 func NewEngine(url string, keywords []string) *Engine {
+	// Retorna um ponteiro para uma Engine
 	return &Engine{url: url, keywords: keywords}
 }
 
-// Remove os espacos do inicio e do final de uma frase "  oi usuario! "-> "oi usuario"
+// Remover os espacos do inicio e do final de uma string
 func trimSpaces(str string) string {
-	// Remove espacos em branco do início da string
+	// Remover espacos em branco do início da string
 	for len(str) > 0 && str[0] == ' ' {
 		str = str[1:]
 	}
-	// Remove espacos em branco do final da string
+	// Remover espacos em branco do final da string
 	for len(str) > 0 && str[len(str)-1] == ' ' {
 		str = str[:len(str)-1]
 	}
@@ -33,44 +32,39 @@ func trimSpaces(str string) string {
 
 // Coleta os resultados
 func (e Engine) Collect() ([]string, error) {
-	// Faz a requisicao http
+
 	response, err := http.Get(e.url)
 	if err != nil {
 		return []string{}, err
 	}
 	defer response.Body.Close()
 
-	// Le o corpo da resposta
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		return []string{}, err
 	}
 
-	// Coloriza todas as keywords que estao no corpo
+	// Colorizar todas as keywords que estao no corpo
 	for _, keyword := range e.keywords {
 		if strings.Contains(string(body), keyword) {
 			body = []byte(strings.ReplaceAll(string(body), keyword, green+keyword+reset))
 		}
 	}
 
-	// Slice para armazenar os resultados
 	var result []string
 
-	// Converte o body para um scanner para facilitar a leitura
+	// Converter o body para um scanner para facilitar a leitura
 	scanner := bufio.NewScanner(strings.NewReader(string(body)))
 
 	// Percorrer cada linha do body
 	for scanner.Scan() {
-		// Linha atual
+
 		line := scanner.Text()
 
 		// Verifica se um dos keywords esta na linha
 		for _, keyword := range e.keywords {
 			if strings.Contains(line, keyword) {
-				// A respectiva linha vai para o resultado
 				result = append(result, trimSpaces(line))
-
-				// Para de verificar a linha atual
 				break
 			}
 		}
