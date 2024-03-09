54513f231348cacf5c04166f73facf4f80ed2fce
Kaic Salomão <imkaicsm@gmail.com>
2024-03-09 13:45:20 -0300

    Reformular o main.go
    Organizar melhor o código e adiciona o suporte para a flag -help

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"scrap/engine"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Uso: scrap website palavra1 palavra2 palavra3 ...")
+const HELP_MSG string = "Uso: scrap <url> <keywords>"
+
+func collectArgs() ([]string, error) {
+	help := flag.Bool("help", false, "exibe a mensagem de ajuda")
+
+	flag.Usage = func() {
+		fmt.Println(HELP_MSG)
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
 		os.Exit(0)
 	}
 
-	var url string
-	var keywords []string
+	args := flag.Args()
 
-	url = os.Args[1]
-
-	// Coletar as keywords passadas em parametro
-	for i := 2; i < len(os.Args); i++ {
-		keywords = append(keywords, os.Args[i])
+	if len(args) < 1 {
+		return nil, errors.New("nao ha argumentos")
 	}
 
-	eng := engine.NewEngine(url, keywords)
+	return args, nil
+}
+
+func main() {
+	args, err := collectArgs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	url := args[0]
+	eng := engine.NewEngine(url, args[1:])
+
 	result, err := eng.CollectResults()
 	if err != nil {
-		fmt.Println("Um erro aconteceu:", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
